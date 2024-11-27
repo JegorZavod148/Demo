524dd2b3fa0e95b3727c4ecbfb41cec5f11c9e5b
JegorZavod148 <egor.kulabukhov69@gmail.com>
2024-11-27 20:09:14 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git remote add origin https://github.com/JegorZavod148/Demo.git
+//git push -u origin main
