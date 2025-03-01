6f258b6faa2fde5075381c995bca8782c3cadf92
JegorZavod148 <egor.kulabukhov69@gmail.com>
2025-03-01 10:04:03 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,31 +1,25 @@
 package org.example;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
 
 public class Main {
 
-    public static void main(String[] args) throws JsonProcessingException {
-        PostgreDB.connect();
+    public static void main(String[] args) {
+        String apiUrl = "https://api.adviceslip.com/advice";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(apiUrl))
+                .GET()
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Response: " + response.body());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
-
-    
-
-//UI ui = new UI();
-//
-//        try {
-//TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-//            botsApi.registerBot(new MyTelegramBot(ui));
-//        } catch (TelegramApiException e) {
-//        e.printStackTrace();
-//        }
-
-
-
-
-
-
